44d2a5b98d29b7b0acb50421a901191e6a4bc2fc
Andrey Tyshchenko <wins00k@gmail.com>
2018-06-01 23:19:01 +0300

    Созданы классы помощники создан класс application который инициализирует всех помощников.

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.groups import GroupsHelper
+from fixture.contacts import ContactsHelper
 
 class Application:
 
@@ -7,117 +9,10 @@ class Application:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # open group page
-        wd.find_element_by_link_text("groups").click()
-        # start group creation
-        wd.find_element_by_name("new").click()
-        # fill in group data
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.open_groups_page()
+        self.groups = GroupsHelper(self)
+        self.contacts = ContactsHelper(self)
 
     def open_homepage(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-
-    def openHomepage(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
-        wd.back()
-
-    def create_new_contact(self, contact):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        wd.find_element_by_name("theform").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[7]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[12]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[12]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[7]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[7]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[13]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[5]//option[13]").click()
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def homepage(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
 
--- test/addgrouptest.py
+++ test/addgrouptest.py
@@ -11,11 +11,11 @@ def app(request):
     
 def test_addgrouptest(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="dadad", header="adada", footer="adda"))
+    app.groups.create_group(Group(name="dadad", header="adada", footer="adda"))
     app.session.logout()
 
 def test2_addgrouptest(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Lol", header="chto", footer="qwerty"))
+    app.groups.create_group(Group(name="Lol", header="chto", footer="qwerty"))
     app.session.logout()
 
