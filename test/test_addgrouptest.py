1e3946cf222d4a31b679e466321e7008b25b1cf7
Andrey Tyshchenko <wins00k@gmail.com>
2018-07-03 13:58:54 +0300

    Добавил генератор данных для групп в json файл

--- test/test_addgrouptest.py
+++ test/test_addgrouptest.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import constant as testdata
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Group(name="", header="",footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_addgrouptest(app, group):
